907612dde75dfac0c46d2ed7f0123d2af91004cc
Martin Palkov <martin.palkov@gmail.com>
2023-11-21 15:38:15 +0100

    iteration 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import "./App.css";
+import dataBase from "./contacts.json";
+console.log(dataBase);
+
+const getFirstNContacts = (array, n) => {
+  const nContactsList = [];
+  for (let i = 0; i < n; i++) {
+    nContactsList.push(array[i]);    
+  }
+  return nContactsList;
+};
 
 function App() {
+  const contactList = getFirstNContacts(dataBase, 5);
+  console.log("app is mounted");
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <tbody>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {
+          contactList.map((element) => {
+            const {pictureUrl, name, popularity, id} = element;
+            return (
+              <tr key={id}>
+                <td><img width="100px" src={pictureUrl} /></td>
+                <td>{name}</td>
+                <td>{popularity}</td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+      </table>
     </div>
   );
 }
